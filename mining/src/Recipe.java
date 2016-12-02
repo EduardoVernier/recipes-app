511605a1873aa30ceed37edc2f0b41698f6fb85c
hermestessaro <hermes.tessaro@gmail.com>
2016-12-02 01:51:29 -0200

    webscraper

--- mining/src/Recipe.java
+++ mining/src/Recipe.java
@@ -1,15 +1,44 @@
+import java.util.*;
+
 
 public class Recipe {
 	public String name;
-	public String[] ingredients;
+	public List<String> ingredients = new ArrayList<String>();
+	public List<String> instructions = new ArrayList<String>();
 	public int yield;
 	public String time;
 	
-	public Recipe(String startName, String[] ingredients2, int startYield, String startTime)
-	{
-		name = startName;
-		ingredients = ingredients2;
-		yield = startYield;
-		time = startTime;
+	
+	
+	public String getName() {
+		return name;
 	}
+	public void setName(String name) {
+		this.name = name;
+	}
+	public List<String> getIngredients() {
+		return ingredients;
+	}
+	public void setIngredients(List<String> ingredients) {
+		this.ingredients = ingredients;
+	}
+	public List<String> getInstructions() {
+		return instructions;
+	}
+	public void setInstructions(List<String> instructions) {
+		this.instructions = instructions;
+	}
+	public int getYield() {
+		return yield;
+	}
+	public void setYield(int yield) {
+		this.yield = yield;
+	}
+	public String getTime() {
+		return time;
+	}
+	public void setTime(String time) {
+		this.time = time;
+	}
+
 }
--- mining/src/WebScrapper.java
+++ mining/src/WebScrapper.java
@@ -3,6 +3,7 @@ import org.jsoup.Jsoup;
 import org.jsoup.nodes.Document;
 import org.jsoup.nodes.Element;
 import org.jsoup.select.Elements;
+import java.util.*;
 
 
 public class WebScrapper {
@@ -11,19 +12,26 @@ public class WebScrapper {
 	   //collects all recipes' links
 	   int page = 1;
 	   String[] links = new String[100];
+	   String[] photos_url = new String[100];
 	   while(page <= 10)
 	   {
 		   String example = "http://tudogostoso.com.br/receitas-populares/"+page+"/";
 		   Document doc = Jsoup.connect(example).userAgent("Chrome").timeout(5000).get();
 		   Elements contents = (Elements) doc.getElementsByClass("box-hover");
+		   Elements photos = (Elements) doc.getElementsByClass("photo");
 		   int i = 0;
 		   String href;
+		   String url;
 	
 		   while(i < 10)
 		   {
 			   Elements tag = contents.get(i).getElementsByTag("a");
 			   href = tag.get(0).attr("href");
 			   links[i+(page-1)*10] = href;
+			   
+			   url = photos.get(i).attr("src");
+			   photos_url[i+(page-1)*10] = url;
+			   
 			   i = i + 1;
 		   }
 		   //System.out.println(contents.size());
@@ -51,23 +59,53 @@ public class WebScrapper {
 		   int yield = Integer.parseInt(temp);
 		   
 		   Elements ingredients_class = doc.getElementsByClass("p-ingredient");
-		   String[] ingredients = new String[ingredients_class.size()];
+		   List<String> ingredients = new ArrayList<String>();
+		   //String[] ingredients = new String[ingredients_class.size()];
 		   int j = 0;
 		   while(j < ingredients_class.size())
 		   {
-			   ingredients[j] = ingredients_class.get(j).text();
+			   ingredients.add(ingredients_class.get(j).text());
 			   j = j + 1;
 		   }
 		   
+		   Elements instructions_class = doc.getElementsByClass("instructions e-instructions");
+		   List<String> instructions = new ArrayList<String>();
+		   
+		   String tempInstructions = new String();
+		   tempInstructions = instructions_class.get(0).text();
+		   //String tempi[] = tempInstructions.split("<ol>|</ol>|<span>|</span>|<li>|</li>|\n\n|<ul>|</ul>|");
+		   
+		  /* for(int k = 0; k<tempInstructions.length(); k++)
+		   {
+			   if(Character.isUpperCase(tempInstructions.charAt(k)))
+			   {
+				   tempInstructions.charAt(k-1) = "\n";
+			   }
+		   }*/
+		   
+		   instructions.add(tempInstructions);
+
+		   
 		  // System.out.println(index+"-"+name);
-		   Recipe newRecipe = new Recipe(name, ingredients, yield, time);
+		   Recipe newRecipe = new Recipe();
+		   newRecipe.setName(name);
+		   newRecipe.setTime(time);
+		   newRecipe.setYield(yield);
+		   newRecipe.setIngredients(ingredients);
+		   newRecipe.setInstructions(instructions);
+		   
 		   recipesList[index] = newRecipe;
 		   index = index + 1;
 	   }
 	   
-	   for (Recipe r : recipesList)
-	   {
-		   System.out.println(r.name);
-	   }
+	   //for (Recipe r : recipesList)
+	   //{
+		 //  System.out.println(r.name);
+	   //}
+	   System.out.println(recipesList[0].name);
+	   System.out.println(recipesList[0].time);
+	   System.out.println(recipesList[0].yield);
+	   System.out.println(recipesList[0].ingredients);
+	   System.out.println(recipesList[0].instructions);
    }
 } 
